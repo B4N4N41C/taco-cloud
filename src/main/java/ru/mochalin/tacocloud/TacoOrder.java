f571b6db5e4ba7168b956ba073225fa26b42ffe7
Nikolay Mochalin <mochalin.nikolay2017@yandex.ru>
2024-06-29 15:02:24 +0300

    Add orderForm.html and finished submit taco order

--- src/main/java/ru/mochalin/tacocloud/TacoOrder.java
+++ src/main/java/ru/mochalin/tacocloud/TacoOrder.java
@@ -11,8 +11,8 @@ public class TacoOrder {
     private String deliveryName;
     private String deliveryStreet;
     private String deliveryCity;
-    private String delivetyState;
-    private String delivetyZip;
+    private String deliveryState;
+    private String deliveryZip;
     private String ccNumber;
     private String ccExpiration;
     private String ccCVV;
