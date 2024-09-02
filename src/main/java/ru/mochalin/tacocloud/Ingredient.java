1f00972ddb8f9fe2598f9ea5bb214293857cf72e
Nikolay Mochalin <mochalin.nikolay2017@yandex.ru>
2024-09-02 20:24:51 +0300

    Added filling the ingredients table using a bean component instead of data.sql queries

--- src/main/java/ru/mochalin/tacocloud/Ingredient.java
+++ src/main/java/ru/mochalin/tacocloud/Ingredient.java
@@ -1,5 +1,7 @@
 package ru.mochalin.tacocloud;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -15,11 +17,27 @@ import lombok.NoArgsConstructor;
 public class Ingredient {
 
     @Id
-    private final String id;
-    private final String name;
-    private final Type type;
+    private String id;
+    private String name;
+    private Type type;
 
     public enum Type{
         WRAP, PROTEIN, VEGGIES, CHEESE, SOUCE
+    }
+
+    @Bean
+    public CommandLineRunner dataLoader(IngredientRepository repo) {
+        return args -> {
+            repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+            repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+            repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+            repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+            repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+            repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+            repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+            repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+            repo.save(new Ingredient("SLSA", "Salsa", Type.SOUCE));
+            repo.save(new Ingredient("SRCR", "Sour Cream", Type.SOUCE));
+        };
     }
 }
